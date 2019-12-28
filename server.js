5fa4333ddd12dfb2ddde77e42ef424049d89a68e
webcrazy216 <53580723+webcrazy216@users.noreply.github.com>
2019-12-28 15:34:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'inde.html'));
 });
 
 app.listen(port);
